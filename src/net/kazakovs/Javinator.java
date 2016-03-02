bf0c63763d152b9dfbac02b89bf113e37f00f4fb
Sergey Kazakov <Sergey Kazakov>
2016-03-03 00:52:49 +0100

    Initial commit

---
+++ src/net/kazakovs/Javinator.java
@@ -0,0 +1,199 @@
+package net.kazakovs;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Javinator implements IJavinator{
+
+    //private Integer session, signature, step;
+    private Response currentResponse;
+    private boolean started=false;
+    private double threshold;
+
+    private static final double DEFAULT_THRESHOLD=95.0;
+
+    private enum _currentAnswer {
+        yes(0), no(1), dontknow(2), probably(3), probablynot(4);
+
+        _currentAnswer(int i) {
+            storrage.put(this, i);
+        }
+        Integer getAnswerId(_currentAnswer ans){
+            return storrage.get(ans);
+        }
+
+        Map<_currentAnswer, Integer> storrage = new HashMap<>();
+    };
+
+    private String currentAnswer="";
+    private String currentQuestion="";
+    private Integer step;
+    private Integer session;
+    private Integer signature;
+
+    public String getCurrentQuestion() {
+        return currentQuestion;
+    }
+
+    public void setCurrentQuestion(String currentQuestion) {
+        this.currentQuestion = currentQuestion;
+    }
+
+    public int getSession() {
+        return session;
+    }
+
+    public int getSignature() {
+        return signature;
+    }
+
+    private ObjectMapper mapper = new ObjectMapper();
+    private final String USER_AGENT = "Mozilla/5.0";
+    private final String CORE_URL = "http://api-en1.akinator.com/ws/";
+
+    public Javinator(double threshold){
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        this.threshold = threshold;
+    }
+
+    public Javinator(){
+        this(DEFAULT_THRESHOLD);
+    }
+
+
+    private double getProgression() {
+        if(this.getCurrentResponse().getParameters().getStep_information()==null)
+            return this.getCurrentResponse().getParameters().getProgression();
+        else
+            return this.getCurrentResponse().getParameters().getStep_information().getProgression();
+    }
+
+    public Boolean haveGuess(){
+        String[] g = null;
+        if(getProgression()>threshold){
+            g = getAllGuesses();
+        }
+        return g!=null && g.length>0 ;
+    }
+
+    public String[] getAllGuesses(){
+        String url = CORE_URL+"list?session=" + this.getSession() +
+                "&signature=" + this.getSignature() +
+                "&step=" + (this.step) +
+                "&mode_question=0";
+        Response response = sendRequest(url);
+        if(response.getParameters().getElements()!=null){
+            String[] out = new String[response.getParameters().getElements().length];
+            int i=0;
+            for(Elements element: response.getParameters().getElements()){
+                out[i++]=element.getElement().getName();
+            }
+            return out;
+        }
+        return null;
+    }
+
+
+    @Override
+    public Integer getStep() {
+        return this.step+1;
+    }
+
+    private Response getCurrentResponse() {
+        return currentResponse;
+    }
+
+    public Integer startSession() {
+
+        String url = CORE_URL+"new_session?partner=1&player=Javinator";
+        Response response = sendRequest(url);
+        this.currentResponse = response;
+        this.session = response.getParameters().getIdentification().getSession();
+        this.signature = response.getParameters().getIdentification().getSignature();
+        this.started=true;
+        this.step=0;
+        this.currentQuestion=getQuestion();
+        return 0;
+
+    }
+
+    private String getQuestion() {
+        if(this.started) {
+            return this.currentResponse.getParameters().getQuestion() != null ?
+                    this.currentResponse.getParameters().getQuestion() :
+                    this.currentResponse.getParameters().getStep_information().getQuestion();
+        }
+        return null;
+    }
+
+    @Override
+    public String sendAnswer(String answer) {
+
+        String url = CORE_URL+"answer?session=" + this.getSession() +
+                "&signature=" + this.getSignature() +
+                "&step=" + (this.step++) +
+                "&answer=" + getAnswerID(answer);
+        this.currentResponse=sendRequest(url);
+        setCurrentQuestion(getQuestion());
+        return getCurrentQuestion();
+
+    }
+
+    @Override
+    public Integer endSession() {
+        this.session=null;
+        this.step=null;
+        return null;
+    }
+
+    private Response sendRequest(String url){
+        try {
+            Response response = mapper.readValue(new URL(url), Response.class);
+//            System.out.println("\t" + url + "\n" + response);
+            return response;
+        } catch (IOException e) {
+            System.out.println("ERROR!!!" + e.getLocalizedMessage());
+        }
+        return null;
+    }
+
+    public static int getAnswerID(String ans){
+        int id = 0;
+        switch (ans.toLowerCase()){
+            case "yes":{}
+            case "y":{
+                id = 0;
+                break;
+            }
+            case "no":{}
+            case "n":{
+                id = 1;
+                break;
+            }
+            case "dontknow":{}
+            case "d":{
+                id = 2;
+                break;
+            }
+            case "probably":{}
+            case "p":{
+                id = 3;
+                break;
+            }
+            case "probablynot":{}
+            case "pn":{
+                id = 4;
+                break;
+            }
+        }
+        return id;
+    }
+
+}
---
+++ src/net/kazakovs/MainGame.java
@@ -0,0 +1,44 @@
+package net.kazakovs;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ * Created by kazak on 3/2/2016.
+ */
+public class MainGame {
+    public static String currentAnswer="";
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+        Javinator ja = new Javinator();
+        System.out.println("Would you like to play a game? (yes/no): ");
+        switch (Javinator.getAnswerID(scan.next())){
+            case 1:
+                System.exit(0);
+            case 0:
+                ja.startSession();
+        }
+
+        while(!currentAnswer.equalsIgnoreCase("exit") && !ja.haveGuess()){
+            System.out.printf("Question %s : %s \n\t", ja.getStep(), ja.getCurrentQuestion());
+            currentAnswer = scan.next();
+            ja.sendAnswer(currentAnswer);
+        }
+        
+        if(ja.haveGuess()){
+            System.out.println("Here are some guesses: ");
+            String[] guesses = ja.getAllGuesses();
+            Arrays.sort(guesses);
+            for(String str : guesses){
+                System.out.println(str);
+            }
+        } else {
+            System.out.println("Unfortunately, Nothing :( ");
+        }
+        ja.endSession();
+
+    }
+
+}
